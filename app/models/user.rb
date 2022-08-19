d806a73b1485f3a9fc02761a13260f151c2f5c75
APTEMmm <artemqrtz@gmail.com>
2022-08-19 19:35:58 +0300

    Added user change and validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,15 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
   has_secure_password
+
+  before_save :downcase_nickname
+
+  validates :email, presence: true, uniqueness: true,
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i }
+  validates :nickname, presence: true, uniqueness: true, format: { with: /\A\w+\z/ }, length: { maximum: 40 }
+
+  def downcase_nickname
+    nickname.downcase!
+  end
 end
