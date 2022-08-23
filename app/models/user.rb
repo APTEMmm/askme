143c2c605031c417d024a1f93dd854c8c907a957
APTEMmm <artemqrtz@gmail.com>
2022-08-23 18:35:58 +0300

    Added question authors

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,7 @@ class User < ApplicationRecord
   validates :nickname, presence: true, uniqueness: true, format: { with: /\A\w+\z/ }, length: { maximum: 40 }
 
   has_many :questions, dependent: :delete_all
+  has_many :asked_questions, class_name: 'Question', foreign_key: :author_id, dependent: :nullify
 
   private
 
